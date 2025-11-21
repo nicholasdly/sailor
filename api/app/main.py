ed98943524aeab3b7dc48ce900e57c2f273fe7ba
Nicholas Ly <nichdly@gmail.com>
2025-11-20 18:27:30 -0600

    dockerfiles and compose file

--- api/app/main.py
+++ api/app/main.py
@@ -11,7 +11,10 @@ sailor = Sailor()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
